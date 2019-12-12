711ee0ea595e730283dad7eab8ddecf3e779adf8
Arkadiusz Smagała <asmagala@outlook.com>
2019-12-12 21:34:17 +0100

    Add match result.

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!')
+} else if (computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Wygrałeś!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('JA wygrałem! :))))');
+} else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('JA wygrałem!')
+} else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('TY wygrałeś!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('TY wygrałeś!!')
+} else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('JA wygrałem!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('REMIS!');
+} else {
+    printMessage('Oszukujesz! Podałeś błędną wartość!!!');
+}
