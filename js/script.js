21a5bbf6a965e8b85d4e55a69f01031df0958f89
asmagala <asmagala@op.pl>
2019-12-19 08:49:08 +0100

    Change - split declaration for buttons.

--- js/script.js
+++ js/script.js
@@ -53,7 +53,12 @@ function playGame(argPlayerInput) {
     displayResult(argComputerMove, argPlayerMove);
 }
 
-document.getElementById("play-rock").addEventListener('click', function() {playGame(1);});
-document.getElementById("play-paper").addEventListener('click', function() {playGame(2);});
-document.getElementById("play-scissors").addEventListener('click', function() {playGame(3);});
+const buttonRock = document.getElementById("play-rock");
+buttonRock.addEventListener('click', function() {playGame(1);});
+
+const buttonPaper = document.getElementById("play-paper");
+buttonPaper.addEventListener('click', function() {playGame(2);});
+
+const buttonScissors = document.getElementById("play-scissors");
+buttonScissors.addEventListener('click', function() {playGame(3);});
 }
